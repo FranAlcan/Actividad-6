4ebdb46308752e4acf140d8dec76b54a308a2bcd
FranAlcan <113629064+FranAlcan@users.noreply.github.com>
2023-02-28 21:57:22 -0400

    Add files via upload

---
+++ Activdad 6/Tarea 1.js
@@ -0,0 +1,9 @@
+let tarifaPorHora = prompt ("¿Cuantos pesos gana por hora?")
+const horasTrabajadasPorDia = 8;
+
+
+const tarifaDiaria = tarifaPorHora * horasTrabajadasPorDia;
+
+
+document.write("La tarifa diaria es: " + tarifaDiaria);
+document.write(" pesos")
